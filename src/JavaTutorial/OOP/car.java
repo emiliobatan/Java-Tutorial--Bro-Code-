fd3e59ef11cc20d077d3b2c1b73759401f018802
Emilio Batan <emiliobatan@ymail.com>
2023-10-11 20:49:18 -0700

    completed OOP tutorial content

---
+++ src/JavaTutorial/OOP/car.java
@@ -0,0 +1,17 @@
+package JavaTutorial.OOP;
+
+public class car {
+    String make = "Porsche";
+    String model = "gt3Rs";
+    int year = 2023;
+    String color = "green";
+    double price = 300000.00;
+
+    void drive() {
+        System.out.println("You drive the car");
+    }
+
+    void brake() {
+        System.out.println("you step on the breaks");
+    }
+}
---
+++ src/JavaTutorial/OOP/oopPractice.java
@@ -0,0 +1,19 @@
+package JavaTutorial.OOP;
+
+public class oopPractice {
+
+    // object = an instance of a class that may contain attributes and methods
+    // ex: (phone, desk, computer, coffee cup)
+
+    public static void main(String[] args) {
+        car porsche = new car();
+
+        // System.out.println(porsche.color);
+        // System.out.println(porsche.make);
+        // System.out.println(porsche.year);
+
+        // porsche.drive();
+        porsche.brake();
+    }
+
+}
