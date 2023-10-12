d9b6c31f39143555be286d6a95dacdbc2465cb9b
Emilio Batan <emiliobatan@ymail.com>
2023-10-11 20:08:28 -0700

    completed all loop content

---
+++ src/JavaTutorial/Loops/whileLoops.java
@@ -0,0 +1,21 @@
+package JavaTutorial.Loops;
+
+import java.util.Scanner;
+
+public class whileLoops {
+    public static void main(String[] args) {
+
+        // while loop = executes a block of code as long as it;s condition remains true
+
+        Scanner scanner = new Scanner(System.in);
+        String name = "";
+
+        while (name.isBlank()) {
+            System.out.println("Enter your name: ");
+            name = scanner.nextLine();
+        }
+
+        System.out.println("Hello " + name);
+        scanner.close();
+    }
+}
