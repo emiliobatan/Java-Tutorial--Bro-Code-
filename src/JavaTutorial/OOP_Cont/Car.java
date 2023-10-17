856cec021b0c23f7c7bae845ea568d28a2a25001
Emilio Batan <emiliobatan@ymail.com>
2023-10-17 15:11:20 -0700

    completed content on objects as method parameters

---
+++ src/JavaTutorial/OOP_Cont/Car.java
@@ -0,0 +1,11 @@
+package JavaTutorial.OOP_Cont;
+
+public class Car {
+    // Class attributes
+    String name;
+
+    // Constructor 
+    Car(String name){
+        this.name = name;
+    }
+}
---
+++ src/JavaTutorial/OOP_Cont/Garage.java
@@ -0,0 +1,11 @@
+package JavaTutorial.OOP_Cont;
+
+public class Garage {
+    
+    // Methods
+
+    // The park method can only take in a car method
+    void park(Car car){
+        System.out.println("The " + car.name + " is parked in the garage");
+    }
+}
